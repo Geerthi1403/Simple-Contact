b93b84b2e150ce67cbd1195f53602c419295dd44
Geerthi1403 <geerphakee1403@gmail.com>
2025-05-22 09:11:34 +0530

    Bugs Fixed

--- Simple Contact Manager/Form1.Designer.cs
+++ Simple Contact Manager/Form1.Designer.cs
@@ -39,9 +39,10 @@
             // btnAdd
             // 
             this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAdd.Location = new System.Drawing.Point(258, 32);
+            this.btnAdd.Location = new System.Drawing.Point(344, 39);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(96, 43);
+            this.btnAdd.Size = new System.Drawing.Size(128, 53);
             this.btnAdd.TabIndex = 0;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
@@ -50,36 +51,40 @@
             // txtName
             // 
             this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtName.Location = new System.Drawing.Point(98, 29);
+            this.txtName.Location = new System.Drawing.Point(131, 36);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(154, 30);
+            this.txtName.Size = new System.Drawing.Size(204, 36);
             this.txtName.TabIndex = 1;
             // 
             // lstDisplay
             // 
             this.lstDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lstDisplay.FormattingEnabled = true;
-            this.lstDisplay.ItemHeight = 25;
-            this.lstDisplay.Location = new System.Drawing.Point(98, 135);
+            this.lstDisplay.ItemHeight = 29;
+            this.lstDisplay.Location = new System.Drawing.Point(131, 166);
+            this.lstDisplay.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lstDisplay.Name = "lstDisplay";
-            this.lstDisplay.Size = new System.Drawing.Size(256, 229);
+            this.lstDisplay.Size = new System.Drawing.Size(340, 265);
             this.lstDisplay.TabIndex = 2;
             // 
             // txtPhone
             // 
             this.txtPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPhone.Location = new System.Drawing.Point(98, 77);
+            this.txtPhone.Location = new System.Drawing.Point(131, 95);
+            this.txtPhone.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtPhone.Name = "txtPhone";
-            this.txtPhone.Size = new System.Drawing.Size(154, 30);
+            this.txtPhone.Size = new System.Drawing.Size(204, 36);
             this.txtPhone.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 32);
+            this.label1.Location = new System.Drawing.Point(16, 39);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.Size = new System.Drawing.Size(81, 29);
             this.label1.TabIndex = 3;
             this.label1.Text = "Name";
             // 
@@ -87,25 +92,28 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(7, 80);
+            this.label2.Location = new System.Drawing.Point(9, 98);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(69, 25);
+            this.label2.Size = new System.Drawing.Size(86, 29);
             this.label2.TabIndex = 3;
             this.label2.Text = "Phone";
             // 
             // frmContacts
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(362, 374);
+            this.ClientSize = new System.Drawing.Size(483, 460);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lstDisplay);
             this.Controls.Add(this.txtPhone);
             this.Controls.Add(this.txtName);
             this.Controls.Add(this.btnAdd);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmContacts";
             this.Text = "Contact";
+            this.Load += new System.EventHandler(this.frmContacts_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Simple Contact Manager/Form1.cs
+++ Simple Contact Manager/Form1.cs
@@ -16,15 +16,42 @@ namespace Simple_Contact_Manager
         {
             InitializeComponent();
         }
-
+        string name;
+        string phone;
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            string name = txtName.Text;
-            string phone = txtPhone.Text;
+            name = txtName.Text;
+            phone = txtPhone.Text;
 
-            // Bug: Doesn't check if fields are empty
-            // Bug: Accepts invalid phone number
-            lstDisplay.Items.Add(name + " - " + phone);
+            if (!string.IsNullOrWhiteSpace(txtName.Text) && !string.IsNullOrWhiteSpace(txtPhone.Text))
+            {
+                if (phone.All(char.IsDigit))
+                {
+                    if (phone.Length >= 10 && phone.Length <= 13)
+                    {
+                        // Bug: Doesn't check if fields are empty
+                        // Bug: Accepts invalid phone number
+                        lstDisplay.Items.Add(name + " - " + phone);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Length should be below 11.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Phone number should in digit.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Fields can't be empty.");
+            }
+        }
+
+        private void frmContacts_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
